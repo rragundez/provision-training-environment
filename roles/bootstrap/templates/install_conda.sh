3b306f253beca9c1576668112aea947a5174c1f8
Rodrigo Agundez <rodrigoagundez@godatadriven.com>
2017-06-23 14:50:50 +0200

    Update setup of conda environment using the environment.yml file

--- roles/bootstrap/templates/install_conda.sh
+++ roles/bootstrap/templates/install_conda.sh
@@ -5,5 +5,27 @@ if [ ! -d "/anaconda" ]; then
     echo "export PYSPARK_PYTHON=/anaconda/bin/python" >> /etc/bash.bashrc
 fi
 
-/anaconda/bin/pip install findspark
-/anaconda/bin/conda install -y seaborn
+/anaconda/bin/conda config --add channels conda-forge
+
+ln_conda=$(grep -n "dependencies:" environment.yml | grep -Eo '^[^:]+')
+ln_pip=$(grep -n "pip:" environment.yml | grep -Eo '^[^:]+')
+ln_prefix=$(grep -n "prefix:" environment.yml | grep -Eo '^[^:]+')
+# conda packages
+if [ -z "$ln_pip" ]; then
+  if [ -z "$ln_prefix" ]; then
+    c=$(awk "NR > ${ln_conda}" environment.yml | cut -c3-)
+  else
+    c=$(awk "NR > ${ln_conda} && NR < ${ln_prefix}" environment.yml | cut -c3-)
+  fi
+else
+  c=$(awk "NR > ${ln_conda} && NR < ${ln_pip}" environment.yml | cut -c3-)
+fi
+/anaconda/bin/conda install -y $c
+
+# pip packages
+if [ -z "$ln_prefix" ]; then
+  p=$(awk "NR > ${ln_pip}" environment.yml | cut -c5-)
+else
+  p=$(awk "NR > ${ln_pip} && NR < ${ln_prefix}" environment.yml | cut -c5-)
+fi
+/anaconda/bin/pip install $p
